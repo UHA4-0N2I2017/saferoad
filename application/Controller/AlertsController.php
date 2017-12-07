315e373a597138263189590218847d0910e8390d
Alvin FREY <alvin.frey@protonmail.ch>
2017-12-07 19:53:44 +0100

    Ajout du contrôleur Alerts

---
+++ application/Controller/AlertsController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Class AlertsController
+ *
+ * Please note:
+ * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
+ * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
+ *
+ */
+
+namespace Mini\Controller;
+
+use Mini\Core\Controller;
+
+class AlertsController extends Controller
+{
+
+    public function index()
+    {
+        $this->display('alerts/index.html.twig', array(
+            'URL' => URL
+        ));
+    }
+
+}
